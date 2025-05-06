e6d9f49c6db1a41a49a9860777337ccce7c7c2fa
Gyanankur Baruah <gyanankurcricket20@gmail.com>
2025-05-06 11:33:12 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,56 +1,59 @@
+"use client";
+
 import { Flex, IconButton, SmartLink, Text } from "@/once-ui/components";
-import { person, social } from "@/app/resources/content";
+import { social } from "@/app/resources/content";
 import styles from "./Footer.module.scss";
 
 export const Footer = () => {
   const currentYear = new Date().getFullYear();
+  const portfolioOwner = "Gyanankur";
 
   return (
     <Flex
       as="footer"
       fillWidth
-      padding="8"
+      padding="16"
       horizontal="center"
+      vertical="center"
+      className={styles.footer}
       mobileDirection="column"
     >
       <Flex
         className={styles.mobile}
         maxWidth="m"
-        paddingY="8"
-        paddingX="16"
-        gap="16"
+        paddingY="16"
+        paddingX="24"
+        gap="20"
         horizontal="space-between"
         vertical="center"
       >
         <Text variant="body-default-s" onBackground="neutral-strong">
           <Text onBackground="neutral-weak">© {currentYear} /</Text>
-          <Text paddingX="4">{person.name}</Text>
+          <Text paddingX="6">{portfolioOwner}</Text>
           <Text onBackground="neutral-weak">
-            {/* Usage of this template requires attribution. Please don't remove the link to Once UI. */}
-            / Build your portfolio with{" "}
-            <SmartLink
-              href="https://once-ui.com/templates/magic-portfolio"
-            >
+            / Crafted with{" "}
+            <SmartLink href="https://once-ui.com/templates/magic-portfolio">
               Once UI
             </SmartLink>
           </Text>
         </Text>
-        <Flex gap="16">
+
+        <Flex gap="20">
           {social.map(
             (item) =>
               item.link && (
-                <IconButton
-                  key={item.name}
+                <Icon key={item.name}
                   href={item.link}
                   icon={item.icon}
                   tooltip={item.name}
                   size="s"
                   variant="ghost"
                 />
-              ),
+              )
           )}
         </Flex>
       </Flex>
+
       <Flex height="80" show="s"></Flex>
     </Flex>
   );
